501e4cd2aa7619e2e2955ad896b944ea410c0075
baldx <mr.ahmad.m2006@gmail.com>
2023-03-22 19:57:58 +0100

    update code

--- script.js
+++ script.js
@@ -1,24 +1,79 @@
+const default_color = "#333333";
+const default_mode ="color";
+const default_size = 16;
+
+let currentColor = default_color;
+let currentMode = default_mode;
+let currentSize = default_size;
+
+function setCurrentColor(newColor) {
+    currentColor = newColor;
+}
+
+function setCurrentMode(newMode) {
+    currentMode = newMode;
+}
+
+function setCurrentSize(newSize) {
+    currentSize = newSize;
+}
+
 let sketchContainer = document.querySelector(".sketch-div");
 const btn = document.querySelectorAll("button");
 
-const pixel = document.createElement("div");
-const sketch = document.querySelector(".sketch-div");
-const pixelBtn = document.querySelectorAll(".pixel-button");
-
+const colorPicker = document.querySelector(".color");
+const colorBtn = document.querySelector(".color-mode");
+const rainbowBtn = document.querySelector(".rainbow-mode");
+const eraserBtn = document.querySelector(".eraser");
+const clearBtn = document.querySelector(".clear");
 const px16 = document.querySelectorAll("#px16");
 const px32 = document.querySelectorAll("#px32");
 const px64 = document.querySelectorAll("#px64");
 const px128 = document.querySelectorAll("#px128");
 
+const pixel = document.createElement("div");
+const sketch = document.querySelector(".sketch-div");
+
+const pixelBtn = document.querySelectorAll(".pixel-button");
+
+colorPicker.oninput = (e) => setCurrentColor(e.target.value);
+colorBtn.onclick = () => setCurrentMode("color");
+rainbowBtn.onclick = () => setCurrentMode("rainbow");
+eraserBtn.onclick = () => setCurrentColor("eraser");
+clearBtn.onclick = () => reloadGrid();
+px16.onclick = (e) => updateSizeValue(e.target.value);
 
 
 
+
+
+
+
+/*
 px16.forEach(button => {
     button.addEventListener("click", () => {
         if (button.onclick = px16) {
-            console.log("16");
+            pixel.classList.add("pixel-16");
+            sketch.appendChild(pixel);
+            sketch.appendChild(pixel.cloneNode(true));
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
+            sketch.appendChild(pixel.cloneNode(true)); 
         }
-    });
+    }, {once: true});
 });
 
 px32.forEach(button => {
@@ -43,4 +98,4 @@ px128.forEach(button => {
             console.log("128");
         }
     });
-});
+}); OLD CODE*/
